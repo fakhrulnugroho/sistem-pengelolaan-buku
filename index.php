ad34fdb26703134bb77dbb654f276f17fa9e95ee
nugrohoff <fakhrl.fanani.nugroho@gmail.com>
2019-12-25 08:39:00 +0700

    add index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+
+require_once 'init.php';
+
+if (!isset($_SESSION['auth'])) {
+	set_flash_message('gagal', 'Anda harus login dulu!');
+	header('Location: login.php');
+}
+
+header('Location: dashboard.php');
+
+?>
